be9b24d86aba6e18cd95bebff74ace25e9743bac
Abdulaziz930 <abdulazizak@code.edu.az>
2022-04-18 17:49:16 +0400

    first commit

---
+++ AP103_18-04-2022_EFPractice/Homework/Program.cs
@@ -0,0 +1,119 @@
+﻿using Homework.Utils;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //await AddEmployee("Yusif Nazarbayov");
+            //await AddEmployeeAsync("Ahmad Jabrayilov");
+            //await AddEmployeeAsync("Elgun Gocayev");
+            //await AddEmployeeAsync("Nergiz Ganiyeva");
+            //await GetEmployeeById(1);
+            //await GetAllEmployeesAsync();
+            //await DeleteEmployeeAsync(5);
+            //await DeleteEmployeeAsync(6);
+            await FilterByNameAsync("u");
+        }
+        public static async Task GetEmployeeByIdAsync(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+            {
+                connection.Open();
+
+                string command = "SELECT Fullname FROM Employees WHERE Id=@id";
+                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@id", id);
+                    string fullName = (await sqlCommand.ExecuteScalarAsync()).ToString();
+                    Console.WriteLine(fullName);
+                }
+            }
+        }
+        public static async Task AddEmployeeAsync(string fullname)
+        {
+            using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+            {
+                connection.Open();
+
+                string command = "INSERT INTO Employees VALUES(@fullname)";
+                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@fullname", fullname);
+                    int result = await sqlCommand.ExecuteNonQueryAsync();
+                    if (result > 0)
+                        Console.WriteLine("employee created");
+                    else
+                        Console.WriteLine("error");
+                }
+            }
+        }
+        public static async Task GetAllEmployeesAsync()
+        {
+            using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+            {
+                connection.Open();
+
+                string command = "SELECT * FROM Employees";
+                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                {
+                    SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Fullname: {reader["Fullname"]}");
+                        }
+                    }
+                }
+            }
+        }
+        public static async Task DeleteEmployeeAsync(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+            {
+                connection.Open();
+
+                string command = "DELETE FROM Employees WHERE Id=@id";
+                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@id", id);
+                    int result = await sqlCommand.ExecuteNonQueryAsync();
+                    if (result > 0)
+                    {
+                        Console.WriteLine("employee deleted");
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+            }
+        }
+        public static async Task FilterByNameAsync(string search)
+        {
+            using (SqlConnection connection = new SqlConnection(Constants.connectionString))
+            {
+                connection.Open();
+
+                string command = "SELECT * FROM Employees WHERE Fullname LIKE '%' + @search + '%'";
+                using (SqlCommand sqlCommand = new SqlCommand(command, connection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@search", search);
+                    SqlDataReader reader = await sqlCommand.ExecuteReaderAsync();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Fullname: {reader["Fullname"]}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AP103_18-04-2022_EFPractice/Homework/Utils/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework.Utils
+{
+    static class Constants
+    {
+        public static string connectionString = "Server=DESKTOP-1QPG03B;Database=AP103CompanyDb;Trusted_Connection=True";
+    }
+}
